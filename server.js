f08b38548f34dd259def6f2a5f83db6863ad1d38
Nick Murphy <nick.murphy01@hotmail.com>
2017-03-01 15:38:12 -0500

    a

--- server.js
+++ server.js
@@ -2,35 +2,37 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static('client'));
-
 var validUrl = require('valid-url');
 
 var urls = 0;
 var urlList = [];
 
 app.get('/new/*', function (req, res) {
-	console.log(req.url);
 	var url=req.url.substring(req.url.indexOf("new")+4);
 	console.log(url);
 	if(!validUrl.isUri(url)){
-		res.send("Error: invalid url");
+		res.end("Error: invalid url");
+	}else{
+		urlList[+urls]=url;
+		var responseObj = {"Original url":url,"Short url":((req.hostname)+'/'+(urls))};
+		urls++;
+		res.writeHead(200,{"content-type": "application/JSON"})
+		res.end(JSON.stringify(responseObj));
 	}
-	urlList[+urls]=url;
-	var responseObj = {"Original url":url,"Short url":((process.env.HOSTNAME)+'/'+(urls))};
-	urls++;
-	res.send(responseObj);
 });
 
 app.get('/*',function(req,res){
 	var url=req.url.substring(1);
 	if(!urlList[url]){
-		res.send("Error: url not found in 'database'");
+		res.writeHead(400,{"content-type": "text/HTML"});
+		res.end("Error: url not found in 'database'");
+	}else{
+		res.writeHead(200,{"content-type": "text/HTML"});
+		res.end("<script>window.location.replace("+"\""+urlList[url]+"\""+")</script>");
 	}
-	res.send("<script>window.location.replace("+"\""+urlList[url]+"\""+")</script>");
 });
 
 app.listen(process.env.PORT||8888, function () {
-	console.log('Example app listening on port %s!',process.env.PORT);
+	console.log('Listening on port %s!',process.env.PORT);
 });
 
